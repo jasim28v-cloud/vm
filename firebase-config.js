39bcf3c833e5193d29bd42dbbfeb969c8394e34e
jasim28v-cloud <jasim28v@gmail.com>
2026-04-04 14:26:23 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,26 +1,33 @@
+// ==================== Firebase Configuration ====================
 const firebaseConfig = {
-    apiKey: "AIzaSyA_sg5Bpwx4B6oGGxV6So1F6fGp_wOhtOw",
-    authDomain: "gosk-47e10.firebaseapp.com",
-    databaseURL: "https://gosk-47e10-default-rtdb.firebaseio.com",
-    projectId: "gosk-47e10",
-    storageBucket: "gosk-47e10.firebasestorage.app",
-    messagingSenderId: "472880925499",
-    appId: "1:472880925499:web:bcc8a3cbc4b00810b4c51e",
-    measurementId: "G-YRMCK7SQWD"
+    apiKey: "AIzaSyDCtfrPPikkAfFy0He_OHxFyiXsYeiTk0o",
+    authDomain: "ftor-440ee.firebaseapp.com",
+    databaseURL: "https://ftor-440ee-default-rtdb.firebaseio.com/",
+    projectId: "ftor-440ee",
+    storageBucket: "ftor-440ee.firebasestorage.app",
+    appId: "1:402606714665:web:2c7701209387c6a89a5138"
 };
 
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
+// Services
 const auth = firebase.auth();
 const db = firebase.database();
 const storage = firebase.storage();
 
+// Cloudinary
 const CLOUD_NAME = 'dk9xej3cf';
 const UPLOAD_PRESET = 'k30_mk';
 
-const AGORA_APP_ID_CALL = '929646610d814d529a06c4081c81325f';
+// Agora
+const AGORA_APP_ID = '4017f66ea15f4ce088e8d8993a072a5b';
 
-const ADMIN_EMAIL = 'jasim30b@gmail.com';
-const ADMIN_PASSWORD = 'bb2314bb';
+// Admin Account
+const ADMIN_EMAIL = 'jasim77v@gmail.com';
+const ADMIN_PASSWORD = 'mm2314mm';
 
-console.log('✅ SPARK - Firebase, Cloudinary & Agora Ready');
+// Site Name
+const SITE_NAME = 'NEXUS';
+
+console.log('✅ NEXUS - Firebase, Cloudinary & Agora Ready');
